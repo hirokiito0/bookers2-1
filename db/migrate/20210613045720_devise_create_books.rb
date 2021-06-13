dcf85072c5aa18391fab95beb39f4a1bcd1494bf
EC2 Default User <ec2-user@ip-172-31-87-241.ec2.internal>
2021-06-13 05:10:30 +0000

    [update]5-10_bookers2_model作成

---
+++ db/migrate/20210613045720_devise_create_books.rb
@@ -0,0 +1,12 @@
+
+class DeviseCreateBooks < ActiveRecord::Migration[5.2]
+  def change
+    create_table :books do |t|
+    
+      t.string :title
+      t.string :body
+      
+      t.timestamps 
+    end
+  end
+end
