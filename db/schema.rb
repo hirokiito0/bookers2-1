54a0ec9bfb3fee328a3c3224db8326afe81e3142
EC2 Default User <ec2-user@ip-172-31-87-241.ec2.internal>
2021-06-13 07:04:46 +0000

    [update]5-10_bookers2_アソシエーション記載

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_13_045720) do
+ActiveRecord::Schema.define(version: 2021_06_13_065855) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "post_images", force: :cascade do |t|
+    t.text "shop_name"
+    t.string "image_id"
+    t.text "caption"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
